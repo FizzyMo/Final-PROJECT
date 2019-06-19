9292ac9ada22226a516553d0dbd2088841c65108
FizzyMo <carisasaenz@gmail.com>
2019-06-18 20:09:06 -0700

    made react work. Now to add polish to files. Just need to add css, html, API, and more javascript to the files

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./componets/Footer";
+import Home from "./componets/pages/Home";
+import Navbar from "./componets/Navbar";
+
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          
+        
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
