6010891fcfa68e4cb261cd7efbc1c4d76708998c
ryangrady311 <ryangrady250@gmail.com>
2019-07-08 19:07:26 -0700

    Revert "welcome, register, login, & footer styling updates"

--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import classnames from "classnames";
-import background from "./images/background.jpeg"
 
 class Register extends Component {
   constructor() {
@@ -58,7 +57,7 @@ class Register extends Component {
     return (
       <div className="ui raised very padded text container segment" style={{ background: 'linear-gradient(to left, white, wheat' }}>
 
-        <h2><i class="large users icon"></i> Please regsiter for a News Glean account and login.</h2>
+        <h2><i class="users icon"></i> Please regsiter for a News Glean account and login</h2>
         <hr />
 
         <div style={{ paddingLeft: '11.250px' }}>
@@ -154,10 +153,9 @@ class Register extends Component {
                 Sign up
                 </button>
             </div>
-            <hr />
-
+            <hr/>
             <h4>Already have an account? <Link to="/login">Log in</Link></h4>
-          
+
           </form>
         </div>
       </div>
