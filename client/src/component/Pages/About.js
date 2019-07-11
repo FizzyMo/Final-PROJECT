11607db60cfab9c728c1d9cbafc658ed6fc69cc9
team awesome <maxj@email.arizona.edu>
2019-07-10 17:29:29 -0700

    major styling for about page

--- client/src/component/Pages/About.js
+++ client/src/component/Pages/About.js
@@ -3,33 +3,38 @@ import Carisa from "../Pages/images/carisa.jpg"
 import Ryan from "../Pages/images/ryan.jpeg"
 import Max from "../Pages/images/max.jpg"
 import Isamar from "../Pages/images/isamar.jpg"
+import background from "../Pages/images/aboutBackground.jpg"
+
 
 
 function About() {
     return (
         <div>
-
-            <div style={{ textAlign: 'center', backgroundImage: 'src=' }} class="ten wide column">
+            <br/>
+            <div style={{ textAlign: 'center', backgroundImage: `url(${background})` }} class="ten wide column">
                 <br />
-                <hr />
-                <h1>NEWS GLEAN</h1>
-                <h2>Project 3 - UA Coding Bootcamp (Jan 2019 Cohort)</h2>
-                <h3><em>"Dynamic news filtering for today's world!"</em></h3>
+                <br />
+                <br />
+                <br />
+                <div style={{ backgroundColor: 'black', opacity: '0.8', paddingTop: '8px', paddingBottom: '8px', fontWeight: 'bolder', borderBottom: '2px solid white' }}>
+                    <h1>NEWS GLEAN</h1>
+                    <h2>Project 3 - UA Coding Bootcamp (Jan 2019 Cohort)</h2>
+                    <h3><em>"Dynamic news filtering for today's world!"</em></h3>
+                </div>
             </div>
-            <br></br>
 
-            <div className='card'>
+            <div className='ui container' style={{ border: '1px, solid grey' }}>
                 <br></br>
-                <p>Welcome to News Glean.  This is a dynamic application that is designed to filter the news by the keywords of your choice.  It will literally change how you consume news.  You can even setup your own profile!</p>
+                <h4>Welcome to News Glean.  This is a dynamic application that is designed to filter the news by the keywords of your choice.  It will literally change how you consume news.  You can even setup your own profile!</h4>
 
-                <p>News Glean is the brainchild of a dynamic team of web developers from the University of Arizona, Carisa Sanez-Videtto, Ryan Grady, and Max Jackson.  Carsia, Ryan, and Max are web developers in the making who will graduate from the university’s Coding Boot Camp in July, 2019.</p>
+                <h4>News Glean is the brainchild of a dynamic team of web developers from the University of Arizona, Carisa Sanez-Videtto, Ryan Grady, and Max Jackson.  Carsia, Ryan, and Max are web developers in the making who will graduate from the university’s Coding Boot Camp in July, 2019.</h4>
 
-                <p>The team has harnessed the power of a dynamic technology framework, React and it skyrockets into production with a Mongo database.  .</p>
-                <p>Finally, with help from ‘NewsApi.org’ our application will render a dynamic list of articles from a balanced journalistic source. </p>
+                <h4>The team has harnessed the power of a dynamic technology framework, React and it skyrockets into production with a Mongo database.  .</h4>
+                <h4>Finally, with help from ‘NewsApi.org’ our application will render a dynamic list of articles from a balanced journalistic source. </h4>
 
-                <p>News Glean utilizes a user-friendly front end design that is modern, sleek and mobile friendly.</p>
-                <p>The styling package of Semantic makes this possible due to it's </p>
-                <p>Semantic UI treats words and classes as exchangeable concepts. Classes use syntax from natural languages like noun/modifier relationships, word order, to link concepts intuitively.</p>
+                <h4>News Glean utilizes a user-friendly front end design that is modern, sleek and mobile friendly.</h4>
+                <h4>The styling package of Semantic makes this possible due to it's </h4>
+                <h4>Semantic UI treats words and classes as exchangeable concepts. Classes use syntax from natural languages like noun/modifier relationships, word order, to link concepts intuitively.</h4>
                 <br />
             </div>
 
@@ -37,6 +42,7 @@ function About() {
             <hr />
             <br />
 
+            {/* Card Deck for Team */}
             <div class="ui centered cards">
                 <div class="ui cards">
                     <div class="card">
@@ -44,9 +50,9 @@ function About() {
                             <img src={Carisa} alt={'Carisa'} />
                         </div>
                         <div class="content">
-                            <div class="header">Carisa</div>
+                            <div class="header">Carisa Saenz-Videtto</div>
                             <div class="meta">
-                                <p>Front End Development Team</p>
+                                <h4>Back End Development Team</h4>
                             </div>
                             <div class="description">
                                 PLACEHOLDER
@@ -67,9 +73,9 @@ function About() {
                             <img src={Ryan} alt={'Ryan'} />
                         </div>
                         <div class="content">
-                            <div class="header">Ryan</div>
+                            <div class="header">Ryan Grady</div>
                             <div class="meta">
-                                <span class="date">Back End Development Team</span>
+                                <h4>Back End Development Team</h4>
                             </div>
                             <div class="description">
                                 PLACEHOLDER
@@ -90,9 +96,9 @@ function About() {
                             <img src={Max} alt={'Max'} />
                         </div>
                         <div class="content">
-                            <div class="header">Max</div>
+                            <div class="header">Max Jackson</div>
                             <div class="meta">
-                                <a>Front End Development Team</a>
+                                <h4>Front End Development Team</h4>
                             </div>
                             <div class="description">
                                 PLACEHOLDER
@@ -113,9 +119,9 @@ function About() {
                             <img src={Isamar} alt={"Isamar"} />
                         </div>
                         <div class="content">
-                            <div class="header">Isamar</div>
+                            <div class="header">Isamar Valdivia</div>
                             <div class="meta">
-                                <a>Front End Development Team</a>
+                                <h4>Front End Development Team</h4>
                             </div>
                             <div class="description">
                                 PLACEHOLDER
@@ -132,7 +138,14 @@ function About() {
                         </div>
                     </div>
                 </div>
+            </div>
+            <br />
+            <hr></hr>
 
+            {/* Video Content */}
+            <h2 style={{ textAlign: 'center' }}>News Glean Demonstration</h2>
+            <div className='card' style={{ textAlign: 'center', padding: '10px', backgroundColor: 'wheat' }}>
+                <iframe width="360" height="215" src="https://www.youtube.com/embed/T4FRqqVzNsw" frameborder="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen='yes'></iframe>
             </div>
 
         </div>
